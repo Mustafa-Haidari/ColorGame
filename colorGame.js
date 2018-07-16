359f59238c8907527f7c9d42dc80cbafce66d8d9
Mustafa Haidari <mhaidarpoor@gmail.com>
2018-07-17 00:57:58 +1000

    Game completed

--- colorGame.js
+++ colorGame.js
@@ -1,32 +1,111 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
 
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener('click', function(){
+    easyBtn.classList.add('selected');
+    hardBtn.classList.remove('selected');
+    h1.style.backgroundColor = 'steelblue';
+    resetButton.textContent = 'New Colors';
+    messageDisplay.textContent = '';
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
+});
+
+hardBtn.addEventListener('click', function(){
+    easyBtn.classList.remove('selected');
+    hardBtn.classList.add('selected');
+    h1.style.backgroundColor = 'steelblue';
+    resetButton.textContent = 'New Colors';
+    messageDisplay.textContent = '';
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = 'block';
+    }
+});
+
+
+resetButton.addEventListener('click', function(){
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i<squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = 'steelblue';
+    this.textContent = 'New Colors';
+    messageDisplay.textContent = '';
+});
+
+
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++){
-	// add initial colors to squares
-	squares[i].style.background = colors[i];
-
-	//add click listeners to squares
-	squares[i].addEventListener("click", function() {
-		//grab color of clicked squares
-		var clickedColor = this.style.background;
-		//compare color to pickedColor
-		if(clickedColor === pickedColor) {
-			alert("Correct!");
-		} else {
-			alert("WRONG!!!");
-		}
-	});
+for(var i = 0; i<squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+    
+    squares[i].addEventListener('click', function(){
+        var clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor){
+            messageDisplay.textContent = 'Correct!';
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = 'PLAY AGAIN?';
+        } else {
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try again!';
+        }
+    });
 }
 
+function changeColors(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random]
+}
+
+function generateRandomColors(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor())
+    }
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return 'rgb('+r+', '+g+', '+b+')';
+}
+
+
+
+
+//return 'rgb('+ Math.floor(Math.random() * 256)+ ', '+ Math.floor(Math.random() * 256)+ ', '+ Math.floor(Math.random() * 256)+ ')';
